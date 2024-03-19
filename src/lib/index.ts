c54883ff436e7e57efdfbb94768dc3dc9aed19c4
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-03-19 21:48:07 +0800

    feat: add whitepaper page example and setup mailer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,40 @@
-// place files you want to import through the `$lib` alias in this folder.
+import nodemailer from 'nodemailer';
+import type { Attachment } from 'nodemailer/lib/mailer';
+
+export class Mailer {
+	private transporter: nodemailer.Transporter;
+
+	constructor() {
+		this.transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.EMAIL,
+				pass: process.env.PASSWORD
+			}
+		});
+	}
+
+async sendMail(
+    to: string,
+    subject: string,
+    text: string,
+    html?: string,
+    attachments?: Attachment[]
+) {
+		const mailOptions: nodemailer.SendMailOptions = {
+			from: process.env.EMAIL,
+			to,
+			subject,
+			text,
+			html,
+			attachments
+		};
+
+		try {
+			await this.transporter.sendMail(mailOptions);
+		} catch (error) {
+			console.error(`Failed to send email: ${error}`);
+			throw error;
+		}
+	}
+}
---
+++ src/routes/whitepaper.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Whitepaper - DmailFi</title>
+</svelte:head>
+
+<div class="pdf-container">
+	<object data="/whitepaper.pdf" type="application/pdf" width="100%" height="100%">
+		<p>
+			It appears you don't have a PDF plugin for this browser. You can <a href="/whitepaper.pdf"
+				>click here to download the PDF file.</a
+			>
+		</p>
+	</object>
+</div>
